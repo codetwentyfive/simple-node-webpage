5cf041301285dbe65411f14ad2f71965fe1d1ebd
code25 <chingisenkhbaatar@gmail.com>
2024-03-24 14:28:46 +0100

    testfiles

--- index.js
+++ index.js
@@ -20,25 +20,24 @@ const server = http.createServer((req, res) => {
       }
     });
   } else if (req.url === "/about" || req.url === "/about.html") {
-    {
-      //serve the html file
-      fs.readFile(path.join(__dirname, "about.html"), (err, data) => {
-        if (err) {
-          res.writeHead(500);
-          res.end("Error loading about.html");
-        } else {
-          res.writeHead(200);
-          res.end(data);
-        }
-      });
-    } else {
-        // Handle 404 - Not Found
-        res.writeHead(404);
-        res.end("Page not found");
+    // Serve the about.html file
+    fs.readFile(path.join(__dirname, "about.html"), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Error loading about.html");
+      } else {
+        res.writeHead(200);
+        res.end(data);
       }
+    });
+  } else {
+    // Handle 404 - Not Found
+    res.writeHead(404);
+    res.end("Page not found");
+  }
 });
 
-//Start the server
+// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
