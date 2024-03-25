6b8948c50034903b16c068a6d1a19c1f284a21d4
code25 <chingisenkhbaatar@gmail.com>
2024-03-25 10:43:53 +0100

    clean path names instead of filenames

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const http = require("http");
-const url = require("url");
 const fs = require("fs");
+const path = require("path");
 
-const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
-  if (err) throw err;
-  return data;
-});
+// Define mappings from clean URLs to file paths
+const routeMappings = {
+  "/": "index.html",
+  "/about": "about.html",
+  "/contact-me": "contact-me.html",
+  "/404": "404.html",
 
-http
-  .createServer(function (req, res) {
-    const q = url.parse(req.url, true);
-    let filename = "";
-    if (q.pathname === "/") {
-      filename = "." + "/index.html";
-    } else {
-      filename = "." + q.pathname;
-    }
+};
 
-    fs.readFile(filename, function (err, data) {
+// Create a server
+http.createServer(function (req, res) {
+  // Parse the URL
+  const url = req.url;
+
+  // Map clean URLs to file paths
+  const filePath = routeMappings[url];
+
+  if (filePath) {
+    // Serve the corresponding file
+    fs.readFile(filePath, function (err, data) {
       if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.write(page404);
-        return res.end();
+        res.writeHead(500);
+        res.end("Error loading file");
       } else {
         res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        return res.end();
+        res.end(data);
       }
     });
-  })
-  .listen(3000);
+  } else {
+    // Handle 404 - Not Found
+    res.writeHead(404);
+    res.end("Page not found");
+  }
+}).listen(8080);
